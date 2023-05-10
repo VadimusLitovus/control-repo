595c33442674514d24881f8fbf74e070483a2997
Vadzim Litouka <vadzim_litouka@epam.com>
2023-05-10 19:57:14 +0200

    Add resources

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,6 @@
 class profile::agent_nodes {
   include dockeragent
+  docker_network {'test_net': ensure => present,}
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
 }
